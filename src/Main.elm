7adee82425bc6487415d2ef1cdc978655dd89ce7
br4ch1st0chr0n3 <br4ch1st0chr0n3@gmail.com>
2021-10-01 11:13:19 +0300

    init

---
+++ src/Main.elm
@@ -0,0 +1,65 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (Html, text)
+import Graph exposing(..)
+import Graph.DOT exposing(..)
+
+-- MAIN
+
+
+
+main : Program Int Model Msg
+main =
+  Browser.element
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+-- MODEL
+
+type alias Model = { 
+  currentTime : Int 
+  
+  }
+
+init : Int -> ( Model, Cmd Msg )
+init currentTime =
+  ( { currentTime = currentTime }
+  , Cmd.none
+  )
+
+
+-- UPDATE
+
+type Msg = NoOp
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update _ model =
+  ( model, Cmd.none )
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  text (String.fromInt model.currentTime)
+
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+  Sub.none
+
+
+{-
+page:
+Add edge:
+Label 1: []
+Label 2: []
+Type: []
+-}
